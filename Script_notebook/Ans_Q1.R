1305af0b2e71224ec33f47cf2ff3184a3d3efdac
Bashar Khan <100842993+basharnawajkhan@users.noreply.github.com>
2022-08-11 17:32:56 +0530

    Add files via upload

---
+++ Script_notebook/Ans_Q1.R
@@ -0,0 +1,9 @@
+Q1 <- read.csv("C:/Users/BASHAR/OneDrive/Documents/Data Science/Assignment/Assignment - 2/Q1.csv")
+
+mean(Q1$Measure.X)
+
+var(Q1$Measure.X)
+
+sd(Q1$Measure.X)
+
+boxplot(Q1$Measure.X)
